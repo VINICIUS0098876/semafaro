7095187f92ab979f2f1487c2d49a9a7d48277b42
VINICIUS0098876 <124823788+VINICIUS0098876@users.noreply.github.com>
2023-08-12 18:41:32 -0300

    Add files via upload

--- semafaro.js
+++ semafaro.js
@@ -1,2 +1,34 @@
 'use strict'
 
+function vermelho(){
+    const vermelho = document.getElementById('vermelho')
+    vermelho.style.backgroundColor = 'red'
+    setTimeout(function(){
+        vermelho.style.backgroundColor = 'rgb(90, 29, 29)'
+        verde()
+    }, 10000)
+}
+
+function amarelo(){
+    const amarelo = document.getElementById('amarelo')
+    amarelo.style.backgroundColor = 'yellow'
+    setTimeout(function(){
+        amarelo.style.backgroundColor = 'rgb(90, 90, 27)'
+        vermelho()
+    }, 5000)
+}
+
+function verde(){
+    const verde = document.getElementById('verde')
+    verde.style.backgroundColor = 'rgb(10, 245, 10)'
+
+    setTimeout(function(){
+        verde.style.backgroundColor = 'rgb(35, 78, 35)'
+        amarelo()
+    }, 5000)
+}
+
+verde()
+
+
+
